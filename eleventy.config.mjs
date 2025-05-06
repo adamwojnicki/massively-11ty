4fc4bb97a431fc833639c0afd5a08f4e8eca22fd
adamwojnicki <wojnicki.adam1@gmail.com>
2025-05-07 00:10:55 +0200

    dynamic nav attempt

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,8 +1,11 @@
-import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
-
 export default async function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets");
-    eleventyConfig.addPlugin(eleventyNavigationPlugin);
+    eleventyConfig.addCollection("navigation", function (collectionApi) {
+        // Return all pages with a navigation property in frontmatter
+        return collectionApi.getAll().filter(function (item) {
+            return item.data.navigation;
+        }).sort((a, b) => (a.data.navOrder || 0) - (b.data.navOrder || 0));
+    });
     return {
         dir: {
             input: "views",

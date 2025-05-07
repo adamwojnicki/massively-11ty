58e86706a8dd0c8dc7babb5e05b826c622d0a50e
adamwojnicki <wojnicki.adam1@gmail.com>
2025-05-07 20:11:35 +0200

    dunamic navigation

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,10 +1,9 @@
 export default async function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets");
     eleventyConfig.addCollection("navigation", function (collectionApi) {
-        // Return all pages with a navigation property in frontmatter
         return collectionApi.getAll().filter(function (item) {
             return item.data.navigation;
-        }).sort((a, b) => (a.data.navOrder || 0) - (b.data.navOrder || 0));
+        }).sort((a, b) => (a.data.navigation.position || 0) - (b.data.navigation.position || 0));
     });
     return {
         dir: {

e2df4980dbe4edbc7648cadc7a01e30e4cb64153
adamwojnicki <wojnicki.adam1@gmail.com>
2025-05-08 19:09:53 +0200

    images imports

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,12 +1,12 @@
 export default async function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.addPassthroughCopy("views/images");
     eleventyConfig.addCollection("navigation", function (collectionApi) {
         return collectionApi.getAll().filter(function (item) {
             return item.data.navigation;
         }).sort((a, b) => (a.data.navigation.position || 0) - (b.data.navigation.position || 0));
     });
     eleventyConfig.addFilter("postDate", (dateObj) => {
-        // Can use toLocaleString the same way we were before
         return dateObj.toLocaleString(undefined, {
             year: "numeric",
             month: "long",
